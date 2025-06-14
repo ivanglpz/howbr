a7b330f52ab2e9cf2050647d1f0d9170b8afd00c
ivanglpz <miltonivan117@gmail.com>
2025-06-14 15:54:26 -0600

    update layout with control border radius, padding and dimension

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,99 +17,88 @@ export default function Home() {
   const [size, setSize] = useState(200);
   return (
     <main
-      className={`${geistSans.className} ${geistMono.className} h-dvh flex flex-col  bg-white`}
+      className={`${geistSans.className} ${geistMono.className} h-dvh flex flex-col justify-center items-center  bg-white`}
     >
-      <section className="flex justify-center items-center h-full">
-        <div className="flex flex-col gap-2 justify-center items-center">
-          <header
-            className="grid grid-cols-3 w-full"
-            style={{
-              width: size,
-            }}
-          >
-            <p className="text-green-500 font-bold text-sm">
-              {borderRadius + padding}px
-            </p>
-            <p className="text-gray-500 font-bold text-sm text-center">
-              {padding}px
-            </p>
-            <p className="text-gray-500"></p>
-          </header>
+      {/*  */}
 
-          <figure
-            className="border-4  flex flex-col items-start border-neutral-300  w-[400px] h-[400px]  relative"
-            style={{
-              padding,
-              borderRadius: borderRadius + padding,
-              width: size,
-              height: size,
-            }}
-          >
-            <aside
-              className="border-t-4 border-l-4 border-green-500 top-[-4px] left-[-4px] absolute  bg-transparent "
-              style={{
-                borderTopLeftRadius: borderRadius + padding,
-                width: size / 2,
-                height: size / 2,
-              }}
-            ></aside>
-            <div
-              className="border-4 p-4 bg-blue-100 border-blue-300 flex justify-center items-center h-full w-full"
-              style={{
-                borderRadius,
-              }}
-            >
-              <p className="text-blue-500 font-bold">{borderRadius}px</p>
-            </div>
-            <aside
-              className="border-t-4 border-l-4 border-blue-500 absolute  bg-transparent "
-              style={{
-                borderTopLeftRadius: borderRadius,
-                width: (size - padding * 2 - 8) / 2,
-                height: (size - padding * 2 - 8) / 2,
-              }}
-            ></aside>
-          </figure>
-          <section className="flex flex-col gap-4  w-[400px]  p-6">
-            <div>
-              <label className="block mb-2 text-black">
-                Border Radius: {borderRadius}px
-              </label>
-              <Slider
-                max={100}
-                step={1}
-                value={[borderRadius]}
-                onValueChange={(value) => setBorderRadius(value[0])}
-              />
-            </div>
-
-            <div>
-              <label className="block mb-2 text-black">
-                Padding: {padding}px
-              </label>
-              <Slider
-                max={size / 2}
-                step={1}
-                value={[padding]}
-                onValueChange={(value) => setPadding(value[0])}
-              />
-            </div>
-            <div>
-              <label className="block mb-2 text-black">
-                Dimension: {size}px
-              </label>
-              <Slider
-                max={500}
-                min={100}
-                step={1}
-                value={[size]}
-                onValueChange={(value) => {
-                  setSize(value[0]);
-                }}
-              />
-            </div>
-          </section>
+      <figure
+        className="border-4  flex flex-col items-start border-neutral-300  w-[400px] h-[400px]  relative"
+        style={{
+          padding,
+          borderRadius: borderRadius + padding,
+          width: size,
+          height: size,
+        }}
+      >
+        <aside
+          className="border-t-4 border-l-4 border-green-500 top-[-4px] left-[-4px] absolute  bg-transparent "
+          style={{
+            borderTopLeftRadius: borderRadius + padding,
+            width: size / 2,
+            height: size / 2,
+          }}
+        ></aside>
+        <div
+          className="border-4 p-4 bg-blue-100 border-blue-300 flex justify-center items-center h-full w-full"
+          style={{
+            borderRadius,
+          }}
+        >
+          <p className="text-blue-500 font-bold">{borderRadius}px</p>
         </div>
+        <aside
+          className="border-t-4 border-l-4 border-blue-500 absolute  bg-transparent "
+          style={{
+            borderTopLeftRadius: borderRadius,
+            width: (size - padding * 2 - 8) / 2,
+            height: (size - padding * 2 - 8) / 2,
+          }}
+        ></aside>
+      </figure>
+      <section className=" absolute top-5 right-5 p-4 flex flex-col gap-4 border bg-white rounded-lg w-[320px]">
+        <div>
+          <label className="block mb-2 text-black">
+            Border Radius: {borderRadius}px
+          </label>
+          <Slider
+            max={100}
+            step={1}
+            value={[borderRadius]}
+            onValueChange={(value) => setBorderRadius(value[0])}
+          />
+        </div>
+
+        <div>
+          <label className="block mb-2 text-black">Padding: {padding}px</label>
+          <Slider
+            max={100}
+            step={1}
+            value={[padding]}
+            onValueChange={(value) => setPadding(value[0])}
+          />
+        </div>
+        <div>
+          <label className="block mb-2 text-black">Dimension: {size}px</label>
+          <Slider
+            max={500}
+            min={100}
+            step={1}
+            value={[size]}
+            onValueChange={(value) => {
+              setSize(value[0]);
+              setPadding(20);
+            }}
+          />
+        </div>
+        <header className="flex flex-row gap-4 w-full">
+          <p className="text-green-500 font-bold text-sm">
+            {borderRadius + padding}px
+          </p>
+          <p className="text-gray-500 font-bold text-sm text-center">
+            {padding}px
+          </p>
+          <p className="text-blue-500 font-bold">{borderRadius}px</p>
+        </header>
       </section>
     </main>
   );

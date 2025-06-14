226c844dab49a32184dd01e9c82a4bc5d927397f
ivanglpz <miltonivan117@gmail.com>
2025-06-14 12:51:26 -0600

    initial project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
+import { Slider } from "@/components/ui/slider";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import { useState } from "react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -12,104 +12,79 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const [borderRadius, setBorderRadius] = useState(10);
+  const [padding, setPadding] = useState(20);
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    <main
+      className={`${geistSans.className} ${geistMono.className} h-dvh flex flex-col  bg-white`}
     >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <section className="flex justify-center items-center h-full">
+        <div className="flex flex-col gap-2">
+          <header className="grid grid-cols-3 ">
+            <p className="text-green-500 font-bold text-sm">
+              {borderRadius + padding}px
+            </p>
+            <p className="text-gray-500 font-bold text-sm text-center">
+              {padding}px
+            </p>
+            <p className="text-gray-500"></p>
+          </header>
+
+          <figure
+            className="border-4  flex flex-col items-start border-neutral-300  w-[400px] h-[400px]  relative"
+            style={{
+              padding,
+              borderRadius: borderRadius + padding,
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div
+              className="border-t-4 border-l-4 border-green-500 top-[-4px] left-[-4px] absolute p-24 bg-transparent "
+              style={{
+                borderTopLeftRadius: borderRadius + padding,
+              }}
+            ></div>
+            <div
+              className="border-4 p-4 bg-blue-100 border-blue-300 flex justify-center items-center h-full w-full"
+              style={{
+                borderRadius,
+              }}
+            >
+              <p className="text-blue-500 font-bold">{borderRadius}px</p>
+            </div>
+            <div
+              className="border-t-4 border-l-4 border-blue-500 absolute p-12 bg-transparent "
+              style={{
+                borderTopLeftRadius: borderRadius,
+              }}
+            ></div>
+          </figure>
+          <section className="flex flex-col gap-4  p-6">
+            <div>
+              <label className="block mb-2 text-black">
+                Border Radius: {borderRadius}px
+              </label>
+              <Slider
+                max={100}
+                step={1}
+                value={[borderRadius]}
+                onValueChange={(value) => setBorderRadius(value[0])}
+              />
+            </div>
+
+            <div>
+              <label className="block mb-2 text-black">
+                Padding: {padding}px
+              </label>
+              <Slider
+                max={100}
+                step={1}
+                value={[padding]}
+                onValueChange={(value) => setPadding(value[0])}
+              />
+            </div>
+          </section>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+    </main>
   );
 }
